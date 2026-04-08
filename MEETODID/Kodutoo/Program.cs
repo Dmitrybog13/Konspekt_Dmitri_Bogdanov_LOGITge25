64b5dc6b9b18b4f1bf0fa3c6c543123925084061
Dmitri Bogdanov <dmitri.bogdanov@tthkee.onmicrosoft.com>
2026-04-08 11:17:10 +0300

    Kodutoo
    Kodutoo tsyklid ja meetodid

---
+++ MEETODID/Kodutoo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _4_Mahukalkulaator
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere tulemast mahukalkulaatorisse!");
+            Console.Write("Kas soovite arvutada kuubi, risttahuka või silindri ruumala? (kuup/risttahukas/silinder): ");
+            string? valik = Console.ReadLine()?.ToLower();
+
+            double ruumala = 0;
+
+            if (valik == "kuup")
+            {
+                Console.Write("Sisesta kuubi külje pikkus: ");
+                double kylg = Convert.ToDouble(Console.ReadLine());
+                ruumala = kylg * kylg * kylg;
+            }
+            else if (valik == "risttahukas")
+            {
+                Console.Write("Sisesta risttahuka pikkus: ");
+                double pikkus = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Sisesta risttahuka laius: ");
+                double laius = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Sisesta risttahuka kõrgus: ");
+                double korgus = Convert.ToDouble(Console.ReadLine());
+                ruumala = pikkus * laius * korgus;
+            }
+            else if (valik == "silinder")
+            {
+                Console.Write("Sisesta silindri põhjaga raadius: ");
+                double raadius = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Sisesta silindri kõrgus: ");
+                double korgus = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Kas silindril on kaas? Sisesta kaane paksus (0 kui puudub): ");
+                double kaanepaksus = Convert.ToDouble(Console.ReadLine());
+                double sisuKorgus = korgus - kaanepaksus;
+                if (sisuKorgus < 0) sisuKorgus = 0;
+                ruumala = Math.PI * raadius * raadius * sisuKorgus;
+            }
+
+            Console.WriteLine($"Teie valitud objekti ruumala on: {ruumala:F2}");
+        }
+    }
+}
---
+++ TSÜKLID/Kodutoo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        Console.WriteLine("Tere tulemast investeerimissimulaatorisse!");
+        Console.Write("Sisesta investeeritav summa: ");
+        double summa = double.Parse(Console.ReadLine());
+
+        Console.Write("Mitu päeva soovid turul olla? ");
+        int päevad = int.Parse(Console.ReadLine());
+
+        double portfell = summa;
+
+        for (int päev = 1; päev <= päevad; päev++)
+        {
+            Console.WriteLine($"\nPäev {päev}:");
+            string[] firmad = { "Tesla", "TransferWise", "Macro$lop" };
+            string valitudFirma = firmad[rnd.Next(firmad.Length)];
+            double kordaja = 0;
+
+            switch (valitudFirma)
+            {
+                case "Tesla":
+                    kordaja = -1.15; // fikseeritud kordaja
+                    break;
+                case "TransferWise":
+                    // juhuslik arv 1–100, jagada 1000-ga ja liita 1
+                    kordaja = 1 + rnd.Next(1, 101) / 1000.0;
+                    break;
+                case "Macro$lop":
+                    // sama kui TransferWise, aga alati negatiivne
+                    kordaja = -(1 + rnd.Next(1, 101) / 1000.0);
+                    break;
+            }
+
+            double päevakasv = portfell * kordaja;
+            portfell += päevakasv;
+
+            Console.WriteLine($"Firma: {valitudFirma}, Hetkekordaja: {kordaja:F3}, Portfelli väärtus: {portfell:F2}");
+
+            if (portfell <= 0)
+            {
+                Console.WriteLine("Portfell on nullis või negatiivne. Sa oled pankrotis.");
+                break;
+            }
+
+            Console.Write("Kas soovid jätkata investeerimist? (jah/ei): ");
+            string jätka = Console.ReadLine().ToLower();
+            if (jätka != "jah")
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine($"\nLõppportfell: {portfell:F2}");
+    }
+}
