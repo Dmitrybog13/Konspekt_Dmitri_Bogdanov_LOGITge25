e5af9667068b646ca03294a5a466f045f1b673a4
Dmitri Bogdanov <dmitri.bogdanov@tthkee.onmicrosoft.com>
2026-05-07 11:11:57 +0300

    Update

---
+++ MEETODID/Kontroltoo/MeetodiToo/MeetodiToo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace MeetodiToo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Tervitus
+            KuvaTervitus();
+
+            // 2. Arvutus: massiivi jagamine (loogika on meetodi sees)
+            double[] arvud = { 500000.1, 100.567, 12.345, 6.66, 0.00033 };
+            double tulemus = ArvutaMassiiviJagatis(arvud);
+            Console.WriteLine($"Lõpptulemus on: {tulemus}");
+            Console.WriteLine("Arvutused tehtud, liigume edasi.");
+
+            // 3. Töötlus: 'a' tähtede lugemine kasutades .Count()
+            Console.WriteLine("\nSisesta oma perekonnanimi:");
+            string nimi = Console.ReadLine() ?? "";
+            int aHulk = LoendaATahed(nimi);
+            Console.WriteLine($"Sinu nimi sisaldab 'a' tähte {aHulk} korda.");
+
+            // 4. Tuvastus: distantsi analüüs
+            Console.WriteLine("\nMitu kilomeetrit sa täna kõndisid?");
+            if (int.TryParse(Console.ReadLine(), out int distants))
+            {
+                Console.WriteLine(AnalüüsiTeekonda(distants));
+            }
+        }
+
+        
+
+        private static void KuvaTervitus()
+        {
+            Console.WriteLine("Tere! Siin on kontrolltöö parandus.");
+        }
+
+       
+        private static double ArvutaMassiiviJagatis(double[] massiiv)
+        {
+            if (massiiv.Length == 0) return 0;
+
+            double vahetulemus = massiiv[0];
+            for (int i = 1; i < massiiv.Length; i++)
+            {
+                vahetulemus /= massiiv[i];
+            }
+            return vahetulemus;
+        }
+
+        
+        private static int LoendaATahed(string tekst)
+        {
+            return tekst.Count(sumbol => char.ToLower(sumbol) == 'a');
+        }
+
+        private static string AnalüüsiTeekonda(int km)
+        {
+            if (km < 0) return "Vigane sisend";
+            if (km == 0) return "Paigalseisuga tervist ei hoia";
+            if (km < 5) return "Tubli tulemus, kontorirotid tavaliselt nii palju ei liigu";
+            if (km < 10) return "Pool linna kõnnib sellega maha";
+            if (km < 15) return "Wow, see võtab üksjagu aega";
+            return "Ära kiirusta!!! >_<";
+        }
+    }
+}
---
+++ TSÜKLID/Kontroltoo/Tsyklitoo/Tsyklitoo/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace Tsyklitoo
+{
+    internal class Start
+    {
+        static void Main(string[] args)
+        {
+            // HARJUTUS 1
+            Console.WriteLine("HARJUTUS 1");
+
+            int vanus;
+
+            do
+            {
+                Console.Write("Sisesta oma vanus: ");
+                vanus = Convert.ToInt32(Console.ReadLine());
+
+            } while (vanus >= 0);
+
+
+            Console.WriteLine("\nVajuta Enter järgmise osa jaoks...");
+            Console.ReadLine();
+
+
+            // HARJUTUS 2
+            Console.WriteLine("HARJUTUS 2");
+
+            int aasta = 0;
+
+            while (aasta < 1000 || aasta > 2025)
+            {
+                Console.Write("Sisesta aasta: ");
+                aasta = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("\nVajuta Enter järgmise osa jaoks...");
+            Console.ReadLine();
+
+
+            // HARJUTUS 3
+            Console.WriteLine("HARJUTUS 3");
+
+            List<string> puuviljad = new List<string>()
+            {
+                "Banaan",
+                "Pirn",
+                "Kiivi",
+                "Apelsin",
+                "Mango"
+            };
+
+            Console.WriteLine("Puuviljade nimekiri:");
+
+            for (int i = 0; i < puuviljad.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {puuviljad[i]}");
+            }
+
+
+            Console.WriteLine("\nVajuta Enter viimase ülesande jaoks...");
+            Console.ReadLine();
+
+
+            // HARJUTUS 4
+            Console.WriteLine("HARJUTUS 4");
+
+            List<int> arvud = new List<int>() { 2, 4, 6, 8 };
+
+            int tulemus = 1;
+
+            foreach (int arv in arvud)
+            {
+                tulemus *= arv;
+                Console.WriteLine($"Praegune korrutis: {tulemus}");
+            }
+        }
+    }
+}
